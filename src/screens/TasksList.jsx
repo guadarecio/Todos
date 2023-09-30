79778f88353772408226ef602f66a20a0dd143f6
g.recio <g.recio@proun.es>
2023-09-30 14:46:13 +0200

    check box added in task list.

--- src/screens/TasksList.jsx
+++ src/screens/TasksList.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { SafeAreaView, View, Text, Button, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TodosContext } from '../context/TodosContext';
+import CheckBox from 'react-native-check-box';
 
 
 const TasksList = () => {
@@ -12,15 +13,30 @@ const TasksList = () => {
         const filterTask = taskDescription.filter((item) => item.description !== description);
         setTaskDescription(filterTask);
     };
+    const handleToggleTask = (taskId) => {
+        setTaskDescription((prevTasks) => prevTasks.map((task) => {
+            if (task.id === taskId) {
+                return { ...task, isChecked: !task.isChecked };
+            }
+            return task;
+        }));
+    };
 
     return (
-        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+        <SafeAreaView style={{ flex: 1 }}>
             <Text>To-Do List</Text>
             <FlatList
                 data={taskDescription}
                 renderItem={({ item }) => (
-                    <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
-                        <Text>{item.description}</Text>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <CheckBox
+                            style={{ flex: 1, padding: 10 }}
+                            onClick={() => handleToggleTask(item.id)}
+                            isChecked={item.isChecked}
+                            rightText={item.description}
+                        />
+
                         <Button title="Delete" onPress={() => handleDeleteTask(item.description)} />
                     </View>
                 )}
