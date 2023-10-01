967fca0479b68664e9e9a7a510fc24dc3cb106ce
g.recio <g.recio@proun.es>
2023-10-01 21:57:16 +0200

    ignore viewproptypes error

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
  * @format
  */
 
+const originalConsoleError = console.error;
+
+console.error = (...args) => {
+  if (args[0].includes('ViewPropTypes will be removed from React Native')) {
+    return;
+  }
+
+  originalConsoleError.apply(console, args);
+};
+
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
